ffe216ceb5167a57f94bdef3e72adf8e4fff3913
imaginebrake <443818839@qq.com>
2018-10-18 01:27:42 +0800

    1018update

---
+++ public/GetTorrInfo.py
@@ -0,0 +1,95 @@
+from bs4 import BeautifulSoup
+import re
+
+def myInt(str):
+    return int(re.sub('[^0-9]','', str))
+
+def GetTorrInfo(torr_html, pt, ptsite_dict):
+
+    html_soup = BeautifulSoup(torr_html, "lxml")
+    torr_list = []
+    title = []
+    uploader = []
+    downloader = []
+    download_link = []
+    print("")
+
+    if pt == "frds":
+        all_torr_soup = html_soup.find(id = "form_torrent")
+        #print(all_torr_soup.prettify())
+        every_torr_soup = all_torr_soup.find_all( "tr", recursive=False)
+        every_torr_soup.remove(every_torr_soup[0])
+        for i in range(len(every_torr_soup)):
+            if not ((every_torr_soup[i].find(class_ = "download")) is None):
+                download_link.append(ptsite_dict[pt]['urls']['link'] + every_torr_soup[i].find(class_ = "download").parent.get("href"))
+                downloader.append(myInt(every_torr_soup[i]("td")[8].string))
+                title.append(every_torr_soup[i]("a")[1].attrs['title'])
+                uploader.append(myInt(every_torr_soup[i]("td")[7].string))
+            else:
+                continue
+
+    if pt == "hdc":
+        every_torr_soup = html_soup.find(class_ = "torrent_list")("tr", recursive=False)
+        every_torr_soup.remove(every_torr_soup[0])
+        for i in range(len(every_torr_soup)):
+            if not ((every_torr_soup[i].find(class_ = "download")) is None):
+                download_link.append(ptsite_dict[pt]['urls']['link'] + every_torr_soup[i].find(class_ = "download").parent.get("href"))
+                downloader.append(myInt(every_torr_soup[i].find(class_ = "t_leech").string))
+                title.append(every_torr_soup[i].find("h3").string)
+                uploader.append(myInt(every_torr_soup[i].find(class_ = "t_torrents").string))
+            else:
+                continue
+
+    if pt == "ttg":
+        every_torr_soup = html_soup.find_all(class_ = re.compile("hover_hr"))
+        for i in range(len(every_torr_soup)):
+            if not ((every_torr_soup[i].find(class_ = "dl_a")) is None):
+                download_link.append(ptsite_dict[pt]['urls']['link'] + every_torr_soup[i].find(class_ = "dl_a").get("href"))
+                downloader.append(myInt(every_torr_soup[i]("td")[8].get_text().split("/\n")[1]))
+                title_len = len(every_torr_soup[i].find(class_ = "name_left")("b"))
+                title.append(every_torr_soup[i].find(class_ = "name_left")("b")[title_len-1].get_text())
+                uploader.append(myInt(every_torr_soup[i]("td")[8].get_text().split("/\n")[0]))
+            else:
+                continue
+
+    if pt == "hdh":
+        all_torr_soup = html_soup.find(id="torrenttable")
+        #print(all_torr_soup.prettify())
+        every_torr_soup = all_torr_soup.find_all( "tr", recursive=False)
+        every_torr_soup.remove(every_torr_soup[0])
+        for i in range(len(every_torr_soup)):
+            if not ((every_torr_soup[i].find(class_ = "download")) is None):
+                download_link.append(ptsite_dict[pt]['urls']['link'] + every_torr_soup[i].find(class_ = "download").parent.get("href"))
+                downloader.append(myInt(every_torr_soup[i]("td")[11].string))
+                title.append(every_torr_soup[i]("a")[1].attrs['title'])
+                uploader.append(myInt(every_torr_soup[i]("td")[10].string))
+            else:
+                continue
+
+    if pt == "opcd":
+        all_torr_soup = html_soup.find(class_="torrents")
+        #print(all_torr_soup.prettify())
+        every_torr_soup = all_torr_soup.find_all( "tr", recursive=False)
+        every_torr_soup.remove(every_torr_soup[0])
+        for i in range(len(every_torr_soup)):
+            if not ((every_torr_soup[i].find(class_ = "download")) is None):
+                download_link.append(ptsite_dict[pt]['urls']['link'] + every_torr_soup[i].find(class_ = "download").parent.get("href"))
+                downloader.append(myInt(every_torr_soup[i]("td")[11].string))
+                title.append(every_torr_soup[i]("a")[1].attrs['title'])
+                uploader.append(myInt(every_torr_soup[i]("td")[10].string))
+            else:
+                continue
+    
+    #print selected torrents
+    for i in range(len(title)):
+        movie_title = title[i]
+        #print("%-10s\n%s\n%-05d  %-05d"%(movie_title,download_link[i],uploader[i],downloader[i]))
+        if (downloader[i]>40 and uploader[i]<5):
+            torr_list.append([pt,movie_title,download_link[i],uploader[i],downloader[i]])
+    if len(torr_list)==0:
+        print("No Torrent has been Selected.")
+    else:
+        print("Selected Torrents:")
+    for torr in torr_list:
+        print("%-10s\n%s\n%-05d  %-05d"%(torr[1],torr[2],torr[3],torr[4]))
+    return torr_list
---
+++ public/TAS.py
@@ -0,0 +1,197 @@
+# -*- coding:utf-8 -*-
+
+import requests
+import re
+import sys
+import os
+import Cookies
+import json
+import GetTorrInfo
+import getopt
+import datetime
+import http.server
+import socketserver
+import ManageFile
+from bs4 import BeautifulSoup
+from multiprocessing import Process
+from apscheduler.schedulers.background import BackgroundScheduler
+from urllib.parse import unquote
+
+#load essential values
+login = False
+pt_choosen = []
+myHeaders = {
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding' : 'gzip, deflate',
+        'Accept-Language' : 'zh-CN,zh;q=0.9,en;q=0.8'
+    } 
+with open("ptsite_info.json",'r') as load_f:
+    ptsite_dict = json.load(load_f)
+
+def httpserver():
+    PORT = 4573
+    web_dir = os.path.join(os.path.dirname(__file__), 'public')
+    os.chdir(web_dir)
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    print("PLEASE DO NOT OPEN THIS IN CHROME, MAY LEAD TO CRASH OF HTTP-SERVER!!!")
+    print("Serving at port", PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+def get_cookies(username = '123', passwd = '', pt = '', login=False):
+    #select get_cookies mod
+    if login:
+        i = 0
+        tryTimes = 0
+        while (i == 0):
+            #let user re-enter the username and passwd
+            if tryTimes != 0:
+                username = input("Enter your username : ")
+                passwd = input("Enter your password : ")
+            i = Cookies.get_cookies( username=username, passwd=passwd, pt = pt, ptsite_dict = ptsite_dict)
+            tryTimes += 1
+        cookies = Cookies.loadcookie(pt = pt, ptsite_dict = ptsite_dict)  # 读取cookie
+    else:
+        cookies = Cookies.loadcookie(pt = pt, ptsite_dict = ptsite_dict)
+        # if load cookies failed
+        if cookies == 0:
+            print("Cookies load failed, try relogin?")
+            if input("if yes, enter 1\n") == "1":
+                username = input("Enter your username : ")
+                passwd = input("Enter your password : ")
+                cookies = get_cookies( username=username, passwd=passwd, pt=pt, login=True)
+                return cookies
+            else:
+                print("wtf??? you dont want to relogin??? bye bye")
+                os._exit(0)
+    return cookies                
+
+def get_torrhtml(pt, cookies, ptsite_dict):
+    torr_url = ptsite_dict[pt]['urls']['torrent']
+    print("Staring Loading Page URL = \"%s\""%torr_url)
+    code = 502 # a initial fail code
+    tryTimes = 0
+    loadFlag = True
+    # load page
+    while (code != 200 and tryTimes < 5):
+        torr_get = requests.get(torr_url, headers=myHeaders, cookies = cookies,)
+        print("Page loaded, Time = %ds"%round(torr_get.elapsed.microseconds / 100000))
+        print("Url = \"%s\" Status_code = %s"%(torr_get.url, torr_get.status_code))
+        code = torr_get.status_code
+        tryTimes += 1
+    if tryTimes>=5:
+        loadFlag = False
+    return (torr_get, loadFlag)
+
+def get_torrents(pt, cookies, torr_list):
+    print("\nDownloading torrents")
+    rssList = []
+    for torr in torr_list:
+        try:
+            #try download torrent
+            torr_file = requests.get(torr[2], headers=myHeaders, cookies = cookies, timeout=20)
+            print("Download success, Time = %ds"%round(torr_file.elapsed.microseconds / 100000))
+            #get filename
+            filename_pattern = re.compile(ptsite_dict[pt]['torr_fn_pattern'])
+            filename = filename_pattern.findall(torr_file.headers['content-disposition'])[0]
+            filename = unquote(filename)
+            print(filename)
+            rssList.append([torr[1],filename])
+            torr_file_path = "public/%s"%filename
+            if not os.path.exists(torr_file_path):
+                with open(torr_file_path, "wb") as savefile:
+                    for data in torr_file.iter_content():
+                        savefile.write(data)
+        except:
+            #skip this torrent if error
+            print("%s\nSkipped"%torr)
+            continue
+    return rssList
+
+def analyze_pt(pt_choosen,ptsite_dict):
+    print("")
+
+    #print time
+    print("Now Time : "+ datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    #analyze each ptsite
+    for pt in pt_choosen:
+
+        try:
+            print("\nStarting Analyzing %s"%ptsite_dict[pt]["fullname"])
+
+            #load cookies
+            cookies = get_cookies(pt = pt)
+
+            #get torrent.php content
+            (torr_get,loadFlag) = get_torrhtml(pt = pt, cookies = cookies, ptsite_dict = ptsite_dict)
+
+            #find hot torrent
+            if loadFlag == True:
+                #get the list of hot torrents
+                torr_list = GetTorrInfo.GetTorrInfo(torr_get.content, pt, ptsite_dict)
+                #download torrents
+                if len(torr_list)>0:
+                    rssList = get_torrents(pt, cookies, torr_list)
+                    ManageFile.AddTorrToRss(rssList)
+            elif loadFlag == False:
+                print("Page Load Failed, this ptsite skipped/n")
+                if input("if you want to remove the cookies saved file, enter 1\n") == "1":
+                    os.remove("cookies_%s.txt"%pt)
+                continue
+                
+        except Exception as e: 
+            print(e)
+            continue
+
+if __name__ == "__main__":
+    help_msg = """                                                                                      
+,@@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@       
+,@@@@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@@@@@@@        
+,@@@@/          @@@@           ,@@@@/                     
+,@@@@/           @@@@          ,@@@@/                     
+,@@@@/           @@@@          ,@@@@/                     
+,@@@@/           @@@@          ,@@@@/                     
+,@@@@/           @@@@          ,@@@@/                     
+,@@@@/          @@@@           ,@@@@/                     
+,@@@@@@@@@@@@@@@@@@            ,@@@@/                     
+,@@@@@@@@@@@@@@@&@             ,@@@@/                                        
+,@@@@/                         ,@@@@/                     
+,@@@@/                         ,@@@@/                                       
+,@@@@/                         ,@@@@/                     
+,@@@@/                         .@@@@/                     
+,@@@@/                         ,@@@@/                     
+,@@@@/                         .@@@@/
+\n\nTAS.py -l <login> -p <ptsite>\n
+Now support ['hdc', 'frds', 'ttg']
+        """
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hrtam:i:l:p:",
+                                   ["help", "login", "ptsite"])
+    except getopt.GetoptError:
+        print(help_msg)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', "help"):
+            print(help_msg)
+            sys.exit()
+        elif opt in ("-l", "--login"):
+            login = True
+        elif opt in ("-p", "--ptsite"):
+            pt_choosen.append(arg)
+    print(pt_choosen)
+    Process_httpserver = Process(target = httpserver)
+    try:
+        Process_httpserver.start()
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(analyze_pt, 'interval', minutes = 15, args=[pt_choosen,ptsite_dict], next_run_time=datetime.datetime.now())
+        scheduler.start()
+        
+    except KeyboardInterrupt:
+        print("Program Ended")
+        os._exit(0)
+    #analyze_pt(pt_choosen,ptsite_dict)
